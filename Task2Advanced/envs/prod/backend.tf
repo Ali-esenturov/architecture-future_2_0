e8dec838293ed7a4c9f88d1244b37dd000173d22
Esenturov <a.esenturov@L-IT169M.local>
2026-04-27 00:07:38 +0600

    fix: use terraform 1.5.7 compatible S3 backend config for Yandex Cloud

--- Task2Advanced/envs/prod/backend.tf
+++ Task2Advanced/envs/prod/backend.tf
@@ -5,7 +5,8 @@ terraform {
     key      = "env/prod/terraform.tfstate"
     region   = "ru-central1"
 
-    skip_region_validation      = true
     skip_credentials_validation = true
+    skip_region_validation      = true
+    force_path_style            = true
   }
 }
